beaae1905e68c1924f71fbdb51ddfd5e830cba07
TimothyChia <timrc96@gmail.com>
2018-04-03 21:38:43 -0500

    Brand new project. Actually called Faces

---
+++ app/src/main/java/com/timothychia/faces/MainActivity.java
@@ -0,0 +1,13 @@
+package com.timothychia.faces;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
